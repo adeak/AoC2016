1fe44766eacbc8e1f99a18d4d0a3d735e603de3b
adeak <adeak@domain>
2016-12-15 09:05:05 +0100

    fix day5 funky

--- day5b_funky.py
+++ day5b_funky.py
@@ -1,6 +1,7 @@
 from hashlib import md5
 import random
 import time
+import string
 
 inputcode = 'ugkcyxxp' # hardwired
 
@@ -17,14 +18,22 @@ def day5b_funky(inp):
         if hexhash.startswith('00000') and hexhash[5].isdigit() and int(hexhash[5]) in range(0,8) and hexhash[5] not in code:
             code[hexhash[5]] = hexhash[6]
         if len(code)==8:
-            funkyprint(code)
+            funkyprint(code,True)
             endprint(code)
             return ''.join(list(zip(*sorted(code.items())))[1])
         ind += 1
 
-def funkyprint(code):
+#def funkyprint(code, done=False): #original
+#    output = ''.join(code[str(k)] if str(k) in code else '{:x}'.format(random.choice(range(16))) for k in range(8))
+#    print('\r' + output + ' Hacking progress [' + '*'*3*len(code) + ' '*3*(8-len(code)) + '] {:3.0f}%'.format(len(code)/8*100),end='')
+
+def funkyprint(code, done = False): #upgrade by @poke
     output = ''.join(code[str(k)] if str(k) in code else '{:x}'.format(random.choice(range(16))) for k in range(8))
+    if not done:
+        for _ in range(random.randrange(2)):
+            print('\r' + ''.join([random.choice(string.digits + string.ascii_letters + string.punctuation) for _ in range(random.randrange(40, 75))]).ljust(76))
     print('\r' + output + ' Hacking progress [' + '*'*3*len(code) + ' '*3*(8-len(code)) + '] {:3.0f}%'.format(len(code)/8*100),end='')
+
 
 def endprint(code):
     endcode = ''.join(list(zip(*sorted(code.items())))[1])
