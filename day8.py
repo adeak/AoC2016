c8915c516562145cb919b4c120712c5c9ac70709
adeak <adeak@domain>
2016-12-15 09:05:18 +0100

    add day8

---
+++ day8.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def day8(inputs):
+    disp = np.zeros((6,50),dtype=bool)
+    for inp in inputs.split('\n'):
+        if inp.startswith('rect'):
+            A,B = map(int,inp.split(' ')[-1].split('x'))
+            disp[:B,:A] = 1
+        elif inp.startswith('rotate row y='):
+            A,B = map(int,inp.split('=')[-1].split(' by '))
+            disp[A,:] = np.roll(disp[A,:],B)
+        elif inp.startswith('rotate column x='):
+            A,B = map(int,inp.split('=')[-1].split(' by '))
+            disp[:,A] = np.roll(disp[:,A],B)
+    print(disp.sum())
+    for k in range(10):
+        print(disp[:,5*k:5*(k+1)].astype(int))
+        print('')
